bbee88e67a98bfc82077fb76d8791b64416b288a
spardutti <luisdamian.sp@gmail.com>
2021-11-27 14:26:23 -0300

    filtering with database

--- client/components/Tours/TourCards.jsx
+++ client/components/Tours/TourCards.jsx
@@ -2,36 +2,45 @@ import Image from "next/image";
 import styles from "../../styles/tours/TourCards.module.scss";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { getAllTours } from "../../store/actions/toursActions";
+import { getAllPackages } from "../../store/actions/Packages/packagesActions";
 
 export const TourCard = () => {
   const dispatch = useDispatch();
   const [packages] = useSelector((state) => state.packages);
 
   useEffect(() => {
-    dispatch(getAllTours());
+    dispatch(getAllPackages());
   }, [dispatch]);
 
   return (
     <div className={styles["cards-container"]}>
       {packages &&
         packages.map((card, index) => {
-          const { destination, type, days, pricePerDay, image } = card;
+          const {
+            packageName,
+            images,
+            description,
+            totalCost,
+            totalDays,
+            destination,
+            type,
+            category,
+          } = card;
           return (
             <div key={index} className={styles.cards}>
               <Image
-                src={image}
+                src={images[0]}
                 layout="responsive"
                 width={10}
                 height={4}
                 objectFit="cover"
               />
               <div className={styles["card-info"]}>
-                <p>{type}</p>
-                <h3>{destination}</h3>
+                <p>{type.typeName}</p>
+                <h3>{packageName}</h3>
                 <div className={styles["info-container"]}>
-                  <p>days {days}</p>
-                  <p>From US$ {pricePerDay * days}</p>
+                  <p>days {totalDays}</p>
+                  <p>From US$ {totalCost}</p>
                 </div>
               </div>
             </div>

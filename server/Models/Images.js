9a279d0bb1b15adf6c3c698f8094250e96fc27aa
aqwsxzzz <jbarretolarrosa@gmail.com>
2021-11-30 10:50:46 -0300

    Deleted images model, routes, etc. Added to destination model

--- server/Models/Images.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-
-const ImagesSchema = new Schema({
-  destination: { type: Schema.Types.ObjectId, ref: "Destination" },
-  images: [],
-});
-
-module.exports = mongoose.model("Images", ImagesSchema);

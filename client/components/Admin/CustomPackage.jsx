423a55208c2e8948ddc0a52e3701ba2e756504fa
Alex Marin Mendez <alexmarinmendez@gmail.com>
2021-12-07 09:49:20 -0500

    feat: add css to /admin

--- client/components/Admin/CustomPackage.jsx
+++ client/components/Admin/CustomPackage.jsx
@@ -1,9 +1,11 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { getAllCustomPackages } from "../../store/actions/customPackage/customPackagesActions";
+import styles from "../../styles/Admin/Input.module.scss";
 
 export const CustomPackage = () => {
     const dispatch = useDispatch();
+    const [show, setShow] = useState(false);
     const [customPackages] = useSelector((state) => state.customPackages);
 
     useEffect(() => {
@@ -11,20 +13,29 @@ export const CustomPackage = () => {
     }, []);
 
 
-      return (
-        <>
-        <h3>CustomPackages</h3>
+    return !show ? (
+        <div onClick={() => setShow(!show)}>
+          <h1 className={styles.drop}>Show Custom Packages</h1>
+        </div>
+    ) : (
+        <div className={styles.form}>
+        <h3 className={styles.drop} onClick={() => setShow(!show)}>
+            Custom Packages
+        </h3>
         {
             customPackages?.length > 0 &&
             customPackages.map(customPackage => 
-                <ul>
-                <li><strong>Client</strong>: {customPackage.clientName} ({customPackage.clientEmail})</li>
-                <li><strong>Destination</strong>: {customPackage.destination} / {customPackage.type} / {customPackage.category}</li>
-                <li>Package for {customPackage.membersQuantity} person(s) - {customPackage.totalDays} days</li>
-                <li>Date of the trip: {customPackage.date}</li>
-                </ul>
+                <>
+                <hr />
+                &nbsp;<br />
+                <div><strong>Client</strong>: {customPackage.clientName} ({customPackage.clientEmail})</div>
+                <div><strong>Destination</strong>: {customPackage.destination} / {customPackage.type} / {customPackage.category}</div>
+                <div>Package for {customPackage.membersQuantity} person(s) - {customPackage.totalDays} days</div>
+                <div>Date of the trip: {customPackage.date}</div>
+                &nbsp;<br />
+                </>
             )
         }
-        </>
+        </div>
     )
 }
--- client/pages/admin.jsx
+++ client/pages/admin.jsx
@@ -28,8 +28,6 @@ export default function Admin({ user }) {
       </div>
       <div className={styles.package}>
         <Package />
-      </div>
-      <div>
         <CustomPackage />
       </div>
       </div>
